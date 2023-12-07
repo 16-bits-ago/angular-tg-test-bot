528976e77250c5d2c23d4b57b777bc90916809d3
Vlad Kapustsin <kapustsin.u@titan-t.com>
2023-12-07 15:52:09 +0200

    bot init

--- main.js
+++ main.js
@@ -1,2 +1,19 @@
-const token = '';
+import {Telegraf, Markup} from 'telegraf';
 
+const token = '6945772404:AAFOThuol5cUfgJUcUi7ZbU8z4nT5DfnTm4';
+const bot = new Telegraf(token);
+const webAppUrl = 'https://google.com/';
+
+bot.command('start', (ctx) => {
+    ctx.reply(
+        'Hello, I\'m a test TG bot! Press on button bellow to get started',
+        Markup.keyboard([
+            Markup.button.webApp(
+                'Click here to open webApp',
+                webAppUrl
+            )
+        ]),
+    );
+});
+
+bot.launch();
