f06a83b69bedba2d5a6517129bb65d9b2b566996
Vlad Kapustsin <kapustsin.u@titan-t.com>
2023-12-10 17:27:11 +0200

    implemented feature to send feedback in bot

--- main.js
+++ main.js
@@ -1,4 +1,5 @@
 import {Telegraf, Markup} from 'telegraf';
+import {message} from 'telegraf/filters';
 
 const token = '6945772404:AAFOThuol5cUfgJUcUi7ZbU8z4nT5DfnTm4';
 const bot = new Telegraf(token);
@@ -7,8 +8,13 @@ const webAppUrl = 'https://angular-tg-app-test-for-bot.web.app';
 bot.command('start', (ctx) => {
     ctx.reply(
         'Hello, I\'m a test TG bot! Press on button bellow to get started',
-        Markup.keyboard([Markup.button.webApp('Click here to open webApp', webAppUrl)]),
+        Markup.keyboard([Markup.button.webApp('Send a feedback', webAppUrl + '/feedback')]),
     );
+});
+
+bot.on(message('web_app_data'), async (ctx) => {
+    const data = ctx.webAppData.data.json();
+    ctx.reply(`Ваше сообщение: ${data?.feedback}` ?? 'Не удалось получить сообщение');
 });
 
 bot.launch();
